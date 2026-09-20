5b601af804e9e10de9d800505da473a0bdbdcfae
Ethan Johnson <ethan.johnson9401@gmail.com>
2026-09-20 18:28:18 -0500

    file cleanup

--- shared/src/commonMain/kotlin/com/ethanjohnson/flipside/GreetingUtil.kt
+++
@@ -1,4 +0,0 @@
-package com.ethanjohnson.flipside
-
-fun sayHello(to: String): String =
-    "Hello, $to!"
--- shared/src/commonMain/kotlin/com/ethanjohnson/flipside/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.ethanjohnson.flipside
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
